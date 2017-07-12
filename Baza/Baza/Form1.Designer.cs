d0add44c69d3109715da88d8a0e349069a295a2b
szafirow <p.szafirowicz1@gmail.com>
2017-07-12 23:09:20 +0200

    test
    test

---
+++ Baza/Baza/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Baza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testConnect = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // testConnect
+            // 
+            this.testConnect.Location = new System.Drawing.Point(12, 26);
+            this.testConnect.Name = "testConnect";
+            this.testConnect.Size = new System.Drawing.Size(75, 33);
+            this.testConnect.TabIndex = 0;
+            this.testConnect.Text = "test";
+            this.testConnect.UseVisualStyleBackColor = true;
+            this.testConnect.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(234, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 30);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Dodaj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(466, 281);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(20, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(259, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(20, 124);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(259, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(20, 184);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(259, 26);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.label2.Location = new System.Drawing.Point(15, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.label3.Location = new System.Drawing.Point(15, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.label4.Location = new System.Drawing.Point(15, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "surname";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(117, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 30);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Wyswietl";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(351, 25);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 30);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Edytuj";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(469, 25);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 30);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Usuń";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(503, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 476);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dane";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(835, 574);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.testConnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.Button testConnect;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Baza/Baza/Form1.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Baza
+{
+    public partial class Form1 : Form
+    {
+        public string conString = "Data Source=LAPTOPSZAFIRKA;Initial Catalog=projekt;Integrated Security=True";
+        public SqlConnection connection;
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new SqlConnection(conString);
+                dataGridView1.ReadOnly = true;
+           
+             
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bład1 " + ex.Message);
+            }
+         
+
+
+           
+            try
+            {
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)
+                {
+                    testConnect.BackColor = Color.Lime;
+                    testConnect.Text = "OK";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bład " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+        }
+
+
+        //test polaczenia
+        private void button3_Click(object sender, EventArgs e)
+        {
+         
+            try
+            {
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("Połączenie poprawne!");
+                    connection.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bład " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        
+        }
+
+        //dodawanie
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)
+                {
+
+
+                    SqlCommand cmd = new SqlCommand("select COUNT(*) from users ", connection);
+                   
+                    
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    if ( String.IsNullOrEmpty(textBox2.Text) || String.IsNullOrEmpty(textBox3.Text) || String.IsNullOrEmpty(textBox4.Text))
+                    {
+                        MessageBox.Show("Nie uzupełniono wszystkich pól!");
+                    }
+                    else if (rd.Read())
+                    {
+                        if (MessageBox.Show("Czy na pewno chcesz wykonać tę czynność?", "Pytanie", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            connection.Close();
+                            connection.Open();
+                            SqlCommand cmd1 =
+                            new SqlCommand("INSERT INTO users (login, name, surname) VALUES ('" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "')", connection);
+                            cmd1.ExecuteNonQuery();
+                            MessageBox.Show("Dodano!");
+                        }
+                        
+                    }
+
+ 
+                    connection.Close();
+
+                }
+               
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bład " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            
+            
+          
+        }
+
+        //wyswietlanie
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+            
+            try
+            {
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)
+                {
+                    SqlCommand cmd = new SqlCommand("select COUNT(*) from users ", connection);
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    if (rd.Read())
+                    {
+                        connection.Close();
+                        connection.Open();
+                        SqlCommand cmd1 =
+                            new SqlCommand("select * from users", connection);
+
+                        SqlDataAdapter sqlDataAdap = new SqlDataAdapter(cmd1);
+
+                        DataTable data = new DataTable();
+                        sqlDataAdap.Fill(data);
+                        dataGridView1.DataSource = data;
+
+
+                        cmd1.ExecuteNonQuery();
+
+                        button4.Text = "Odśwież";
+                        button4.BackColor = Color.Silver;
+                        button5.Visible = true;
+                        button6.Visible = true;
+
+                       // MessageBox.Show("Wyświetlono dane!");
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Pusta tabela!");
+                    }
+
+                    connection.Close();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bład " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        //update
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)
+                {
+                    SqlCommand cmd = new SqlCommand("select COUNT(*) from users", connection);
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    if (rd.Read())
+                    {
+                        
+
+                        if (MessageBox.Show("Czy na pewno chcesz wykonać tę czynność?", "Pytanie", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            connection.Close();
+                        connection.Open();
+                            SqlCommand cmd1 =
+                                new SqlCommand("UPDATE users SET login = '" + textBox2.Text + "' , name ='" + textBox3.Text + "', surname = '" + textBox4.Text + "' WHERE id='" + dataGridView1.SelectedRows[0].Cells[0].Value.ToString() + "' ", connection);
+
+                            SqlDataAdapter sqlDataAdap = new SqlDataAdapter(cmd1);
+
+                           cmd1.ExecuteNonQuery();
+                          
+                            MessageBox.Show("Edytowano dane!");
+                           
+                        }
+                        
+                       
+                    }
+                    else
+                    {
+                        MessageBox.Show("Pusta tabela!");
+                    }
+
+                    connection.Close();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bład " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+
+
+     
+
+        //usuwanie
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)
+                {
+                    SqlCommand cmd = new SqlCommand("select COUNT(*) from users", connection);
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    if (rd.Read())
+                    {
+
+
+                        if (MessageBox.Show("Czy na pewno chcesz wykonać tę czynność?", "Pytanie", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            connection.Close();
+                            connection.Open();
+                            SqlCommand cmd1 =
+                                new SqlCommand("DELETE FROM users WHERE id='" + dataGridView1.SelectedRows[0].Cells[0].Value.ToString() + "' ", connection);
+
+                            SqlDataAdapter sqlDataAdap = new SqlDataAdapter(cmd1);
+
+                            cmd1.ExecuteNonQuery();
+
+
+                            MessageBox.Show("Usunieto dane!");
+                        }
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Pusta tabela!");
+                    }
+
+                    connection.Close();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bład " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            try
+            {
+                textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+                textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+                textBox4.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bład " + ex.Message);
+            }
+           
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+                textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+                textBox4.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bład " + ex.Message);
+            }
+        }
+
+        
+
+       
+
+       
+    }
+}
